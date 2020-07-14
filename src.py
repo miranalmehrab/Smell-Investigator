2839fef0e53512c15a94e4474a85372815a8d7d5
MiranAlMehrab <bsse0931@iit.du.ac.bd>
2020-07-14 10:17:57 +0600

    code splitter added

--- src.py
+++ src.py
@@ -111,25 +111,25 @@
 # z = np.linspace(1, 500, N)
 
 
-# def clamp(a, amin, amax):
-#     return np.max([amin, np.min([a, amax])])
+def clamp(a, amin, amax):
+    return np.max([amin, np.min([a, amax])])
 
 
-# def equation7(z):
-#     zabs = np.abs(z)
-#     val = 10. / (1e-5 + np.power(zabs/5.0, 2.0) + np.power(zabs/200.0, 6.0))
-#     return clamp(val, 1e-2, 3e3)
+def equation7(z):
+    zabs = np.abs(z)
+    val = 10. / (1e-5 + np.power(zabs/5.0, 2.0) + np.power(zabs/200.0, 6.0))
+    return clamp(val, 1e-2, 3e3)
 
 
-# def equation8(z):
-#     zabs = np.abs(z)
-#     val = 10. / (1e-5 + np.power(zabs/5.0, 3.0) + np.power(zabs/200.0, 6.0))
-#     return clamp(val, 1e-2, 3e3)
+def equation8(z):
+    zabs = np.abs(z)
+    val = 10. / (1e-5 + np.power(zabs/5.0, 3.0) + np.power(zabs/200.0, 6.0))
+    return clamp(val, 1e-2, 3e3)
 
 
-# def equation9(z):
-#     val = 0.001/(1e-5 + np.power(np.abs(z)/200.0, 4.0))
-#     return clamp(val, 1e-2, 3e3)
+def equation9(z):
+    val = 0.001/(1e-5 + np.power(np.abs(z)/200.0, 4.0))
+    return clamp(val, 1e-2, 3e3)
 
 
 # zw = np.array([[equation7(zi), equation8(zi), equation9(zi)] for zi in z])
